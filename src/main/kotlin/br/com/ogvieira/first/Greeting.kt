070cfd27b99899b204db3a071f1ce297cc5d683a
ogvieira <osmargv100@gmail.com>
2025-03-22 15:19:15 -0300

    primeiro commit

---
+++ src/main/kotlin/br/com/ogvieira/first/Greeting.kt
@@ -0,0 +1,4 @@
+package br.com.ogvieira.first
+
+class Greeting(val id:Long, val content:String) {
+}
---
+++ src/main/kotlin/br/com/ogvieira/first/GreetingController.kt
@@ -0,0 +1,17 @@
+package br.com.ogvieira.first
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+
+@RestController
+class GreetingController {
+
+    val counter:AtomicLong =  AtomicLong();
+
+    @RequestMapping("/greeting")
+    fun greeting():Greeting{
+        return  Greeting(counter.incrementAndGet(),"Hello Kotlin");
+    }
+}
