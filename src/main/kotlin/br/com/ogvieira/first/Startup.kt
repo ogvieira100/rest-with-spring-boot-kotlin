fd18f315727213cb5069f6a0ccf825a5a3f171af
ogvieira <osmargv100@gmail.com>
2025-03-22 11:28:00 -0300

    primeiro commit

---
+++ src/main/kotlin/br/com/ogvieira/first/Startup.kt
@@ -0,0 +1,11 @@
+package br.com.ogvieira.first
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
