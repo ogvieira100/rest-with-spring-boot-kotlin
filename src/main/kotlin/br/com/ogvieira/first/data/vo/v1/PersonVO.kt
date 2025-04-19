53de413f06e033d5ddc86cd2822beaa25b5d360e
ogvieira <osmargv100@gmail.com>
2025-04-19 18:27:41 -0300

    lidando com propriedades json

--- src/main/kotlin/br/com/ogvieira/first/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/ogvieira/first/data/vo/v1/PersonVO.kt
@@ -1,12 +1,19 @@
 package br.com.ogvieira.first.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 
 
+//@JsonPropertyOrder("id", "first_name", "lastName", "last_name", "gender")
 data class PersonVO (
-
+                    @field:JsonProperty("id")
                     var id: Long = 0,
+                    @field:JsonProperty("firstName")
                     var firstName: String = "",
+                    @field:JsonProperty("lastName")
                     var lastName: String = "",
+                    @field:JsonProperty("address")
                     var address: String = "",
+                    @field:JsonProperty("gender")
                     var gender: String = ""
                 )
